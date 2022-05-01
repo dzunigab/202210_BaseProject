006fdb079adc05552877cb8995b21b26553972de
David Zúñiga <d.zunigab@uniandes.edu.co>
2022-05-01 18:33:21 -0500

    feat: Creacion del servicio vehiculos

---
+++ src/app/vehiculo/vehiculo.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { Vehiculo } from './vehiculo';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class VehiculoService {
+  private urlAPI: string = environment.URL_API;
+
+  constructor(private http: HttpClient) {}
+
+  getVehiculos(): Observable<Vehiculo[]> {
+    return this.http.get<Vehiculo[]>(this.urlAPI);
+  }
+}
