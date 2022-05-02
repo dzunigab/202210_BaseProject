09726d0adcec3d6a969fd1f85efc930b17867d0d
David Zúñiga <d.zunigab@uniandes.edu.co>
2022-05-01 22:38:37 -0500

    feat: adicion de funcion para contar las marcas repetidas en la tabla, creacion clase Cantidad

---
+++ src/app/vehiculo/cantidad.ts
@@ -0,0 +1,12 @@
+export class Cantidad {
+
+  marca: string;
+  cantidad: number;
+  constructor(
+    cantidad: number,
+    marca: string
+  ) {
+    this.cantidad = cantidad;
+    this.marca = marca;
+  }
+}
--- src/app/vehiculo/listat-vehiculos/listat-vehiculos.component.ts
+++ src/app/vehiculo/listat-vehiculos/listat-vehiculos.component.ts
@@ -1,21 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { Cantidad } from '../cantidad';
 import { Vehiculo } from '../vehiculo';
 import { VehiculoService } from '../vehiculo.service';
 
 @Component({
   selector: 'app-listat-vehiculos',
   templateUrl: './listat-vehiculos.component.html',
-  styleUrls: ['./listat-vehiculos.component.css']
+  styleUrls: ['./listat-vehiculos.component.css'],
 })
 export class ListatVehiculosComponent implements OnInit {
-  vehiculos: Array<Vehiculo> = []
-
-  constructor(private vehiculoService: VehiculoService) { }
-  getVehiculos():void{
-    this.vehiculoService.getVehiculos().subscribe(vehiculos => this.vehiculos = vehiculos)
+  vehiculos: Vehiculo[] = [];
+  grupoMarcas: Cantidad[] = [];
+  constructor(private vehiculoService: VehiculoService) {}
+  getVehiculos(): void {
+    this.vehiculoService.getVehiculos().subscribe((vehiculos) => {
+      this.vehiculos = vehiculos;
+      this.validateMarca(vehiculos);
+    });
+  }
+  validateMarca(objectArray: Vehiculo[]): void {
+    let arrValidacion: Cantidad[] = [];
+    objectArray.map((item) => {
+      let k: number = 0;
+      let valida: Cantidad = arrValidacion.find((value, index) => {
+        if (value.marca == item.marca) {
+          k = index;
+          return value;
+        }
+        return undefined;
+      }) || { marca: '', cantidad: 1 };
+      if (valida.marca == '') {
+        valida.marca = item.marca;
+        valida.cantidad = 1;
+        arrValidacion.push(valida);
+      } else {
+        arrValidacion[k].cantidad = arrValidacion[k].cantidad + 1;
+      }
+    });
+    this.grupoMarcas = arrValidacion
   }
   ngOnInit() {
-    this.getVehiculos()
+    this.getVehiculos();
   }
-
 }
